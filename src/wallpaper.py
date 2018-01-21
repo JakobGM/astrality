df023fb731fcd6bfc3a74d300ccf1b67d89f0249
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-01-21 02:23:43 +0100

    Check exit code of feh

--- src/wallpaper.py
+++ src/wallpaper.py
@@ -19,8 +19,15 @@ def update_wallpaper(config: Config, period: str) -> None:
 def exit_feh(config) -> None:
     this_file = os.path.realpath(__file__)
     parent_dir = os.path.join(*this_file.split('/')[:-1])
-    subprocess.Popen([
+    feh_process = subprocess.Popen([
         'feh',
         config['wallpaper'].get('feh-option', '--bg-scale'),
         '/' + parent_dir + '/solid_black_background.jpeg',
     ])
+    try:
+        exit_code = feh_process.wait(timeout=10)
+    except subprocess.TimeoutExpired:
+        print('feh is using unusually long time to set the background image.')
+    finally:
+        if exit_code != 0:
+            print(f'feh exited with error code: {exit_code}.')
