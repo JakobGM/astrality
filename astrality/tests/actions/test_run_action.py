b8e636b102bbaf4a06805643b369179dd47ec0fa
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-04-13 22:48:13 +0200

    Use CompileAction in astrality.module

--- astrality/tests/actions/test_run_action.py
+++ astrality/tests/actions/test_run_action.py
@@ -1,5 +1,7 @@
 """Tests for RunAction."""
 
+import logging
+import os
 from pathlib import Path
 
 from astrality.actions import RunAction
@@ -64,9 +66,9 @@ def test_run_timeout_specified_in_action_block(tmpdir):
     _, result = run_action.execute(default_timeout=0)
     assert result == 'hi'
 
-def test_run_timeout_specified_in_execute(tmpdir):
+def test_run_timeout_specified_in_execute(tmpdir, caplog):
     """
-    Run actions can time out.
+    Run actions can time out, and should log this.
 
     The the option `timeout` is not specified, use `default_timeout` argument
     instead.
@@ -78,7 +80,11 @@ def test_run_timeout_specified_in_execute(tmpdir):
         replacer=lambda x: x,
         context_store={},
     )
+
+    caplog.clear()
     _, result = run_action.execute(default_timeout=0.05)
+
+    assert 'used more than 0.05 seconds' in caplog.record_tuples[1][2]
     assert result == ''
 
     run_action = RunAction(
@@ -89,3 +95,40 @@ def test_run_timeout_specified_in_execute(tmpdir):
     )
     _, result = run_action.execute(default_timeout=0.2)
     assert result == 'hi'
+
+def test_running_shell_command_with_non_zero_exit_code(caplog):
+    """Shell commands with non-zero exit codes should log this."""
+    run_action = RunAction(
+        options={'shell': 'thiscommandshould not exist', 'timeout': 2},
+        directory=Path('/'),
+        replacer=lambda x: x,
+        context_store={},
+    )
+    caplog.clear()
+    run_action.execute()
+    assert 'not found' in caplog.record_tuples[1][2]
+    assert 'non-zero return code' in caplog.record_tuples[2][2]
+
+def test_running_shell_command_with_environment_variable(caplog):
+    """Shell commands should have access to the environment."""
+    run_action = RunAction(
+        options={'shell': 'echo $USER', 'timeout': 2},
+        directory=Path('/'),
+        replacer=lambda x: x,
+        context_store={},
+    )
+
+    caplog.clear()
+    run_action.execute()
+    assert caplog.record_tuples == [
+        (
+            'astrality.actions',
+            logging.INFO,
+            'Running command "echo $USER".',
+        ),
+        (
+            'astrality',
+            logging.INFO,
+            os.environ['USER'] + '\n',
+        )
+    ]
