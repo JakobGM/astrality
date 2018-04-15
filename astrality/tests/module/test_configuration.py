1e6b028db24b9a571d7b650bf548f2f5fbf4ad6a
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-04-15 20:04:48 +0200

    Remove obsoleted test

--- astrality/tests/module/test_configuration.py
+++ astrality/tests/module/test_configuration.py
@@ -5,67 +5,6 @@ import pytest
 
 from astrality.module import GlobalModulesConfig, Module, ModuleManager
 
-# TODO: Delete this after populate_event_blocks is made uneccessary
-@pytest.mark.skip(reason='No longer in use after module/refactor')
-def test_that_module_configuration_is_processed_correctly_before_use(
-    test_config_directory,
-):
-    """
-    Test that all list item configurations can be given as single strings,
-    and that missing configuration options are inserted.
-    """
-    module_config = {'module/A': {
-        'on_startup': {
-            'run': 'echo hi!',
-        },
-        'on_event': {
-            'import_context': {'from_file': '/test'},
-            'run': ['echo 1', 'echo 2'],
-            'trigger': {'block': 'on_modified', 'path': '/some/file'},
-        },
-        'on_modified': {
-            '/some/file': {
-                'compile': {'template': '/some/template'},
-            },
-        },
-    }}
-
-    module = Module(
-        module_config=module_config,
-        module_directory=test_config_directory,
-    )
-
-    processed_config = {
-        'on_startup': {
-            'run': ['echo hi!'],
-            'compile': [],
-            'import_context': [],
-            'trigger': [],
-        },
-        'on_event': {
-            'import_context': [{'from_file': '/test'}],
-            'run': ['echo 1', 'echo 2'],
-            'compile': [],
-            'trigger': [{'block': 'on_modified', 'path': '/some/file',}],
-        },
-        'on_exit': {
-            'run': [],
-            'compile': [],
-            'import_context': [],
-            'trigger': [],
-        },
-        'on_modified': {
-            '/some/file': {
-                'compile': [{'template': '/some/template'}],
-                'run': [],
-                'import_context': [],
-                'trigger': [],
-            },
-        },
-    }
-    assert module.module_config == processed_config
-
-
 def test_modules_config_explicitly_enabled_modules(
     test_config_directory,
 ):
