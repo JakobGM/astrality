be2b1fc7ef6d0fe6379ed8955923aaea3494d00a
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-05-21 22:06:41 +0200

    Add module manager scoped cleanup test

--- astrality/tests/module/module_manager/test_persisting_created_files.py
+++ astrality/tests/module/module_manager/test_persisting_created_files.py
@@ -43,3 +43,36 @@ def test_that_created_files_are_persisted(method, create_temp_files):
     created_files = CreatedFiles()
     assert created_files.by(module='A') == [target1, target2]
     assert created_files.by(module='B') == [target3]
+
+    # Now we should be able to cleanup the created files
+    assert target1.exists()
+    assert target2.exists()
+    assert target3.exists()
+
+    # First let's see if dry run is respected
+    created_files.cleanup(module='A', dry_run=True)
+    assert target1.exists()
+    assert target2.exists()
+    assert target3.exists()
+    assert created_files.by(module='A') == [target1, target2]
+    assert created_files.by(module='B') == [target3]
+
+    # Now see if we can cleanup module A and let B stay intact
+    created_files.cleanup(module='A')
+    assert not target1.exists()
+    assert not target2.exists()
+    assert target3.exists()
+    assert created_files.by(module='A') == []
+    assert created_files.by(module='B') == [target3]
+
+    # Now all files should be cleaned
+    created_files.cleanup(module='B')
+    assert not target3.exists()
+    assert created_files.by(module='A') == []
+    assert created_files.by(module='B') == []
+
+    # Let's see if it has been properly persisted too
+    del created_files
+    created_files = CreatedFiles()
+    assert created_files.by(module='A') == []
+    assert created_files.by(module='B') == []
