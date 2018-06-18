e37ffd405d85084641966bd7a7440319008020f5
Jakob Gerhard Martinussen <jakobgm@gmail.com>
2018-06-18 16:42:12 +0200

    Add docstrings for config.py tests

--- astrality/tests/config/test_config.py
+++ astrality/tests/config/test_config.py
@@ -1,3 +1,4 @@
+"""Tests for locating and parsing YAML configuration files."""
 import os
 from pathlib import Path
 from shutil import rmtree
@@ -14,6 +15,7 @@ from astrality.utils import compile_yaml
 
 @pytest.fixture
 def dummy_config():
+    """Return dummy configuration YAML file."""
     test_conf = Path(__file__).parents[1] / 'test_config' / 'test.yml'
     return compile_yaml(
         path=test_conf,
@@ -22,31 +24,38 @@ def dummy_config():
 
 
 class TestAllConfigFeaturesFromDummyConfig:
-    def test_normal_variable(self, dummy_config):
-        assert dummy_config['section1']['var1'] == 'value1'
+    """Tests for .utils.compile_yaml."""
 
-    def test_variable_interpolation(self, dummy_config):
+    def test_normal_variable(self, dummy_config):
+        """String literals should be interpreted without modifications."""
+        assert dummy_config['section1']['var1'] == 'value1'
         assert dummy_config['section1']['var2'] == 'value1/value2'
         assert dummy_config['section2']['var3'] == 'value1'
 
     def test_empty_string_variable(self, dummy_config):
+        """Empty strings  should be representable."""
         assert dummy_config['section2']['empty_string_var'] == ''
 
     def test_non_existing_variable(self, dummy_config):
+        """Non-existing keys should not have defaults but raise instead."""
         with pytest.raises(KeyError):
             assert dummy_config['section2']['not_existing_option'] \
                 is None
 
     def test_environment_variable_interpolation(self, dummy_config):
+        """Environment variables should be interpolated with Jinja syntax."""
         assert dummy_config['section3']['env_variable'] \
             == 'test_value, hello'
 
 
 class TestResolveConfigDirectory:
+    """Tests for .config.resolve_config_directory."""
+
     def test_setting_directory_using_application_env_variable(
         self,
         monkeypatch,
     ):
+        """ASTRALITY_CONFIG_HOME should override XDG_CONFIG_HOME."""
         monkeypatch.setattr(
             os,
             'environ',
@@ -58,6 +67,7 @@ class TestResolveConfigDirectory:
         assert resolve_config_directory() == Path('/test/dir')
 
     def test_setting_directory_using_xdg_directory_standard(self, monkeypatch):
+        """The XDG_CONFIG_HOME environment variable should be respected."""
         monkeypatch.setattr(
             os,
             'environ',
@@ -71,21 +81,28 @@ class TestResolveConfigDirectory:
         self,
         monkeypatch,
     ):
+        """In the absence of XDG_CONFIG_HOME, the standard location is used."""
         monkeypatch.setattr(os, 'environ', {})
         assert resolve_config_directory() \
             == Path('~/.config/astrality').expanduser()
 
 
 class TestCreateConfigDirectory:
+    """Tests for creating configuration content, used be the CLI entrypoint."""
+
     def test_creation_of_empty_config_directory(self):
+        """An empty configuration directory can be created."""
         config_path = Path('/tmp/config_test')
         config_dir = create_config_directory(path=config_path, empty=True)
+
         assert config_path == config_dir
         assert config_dir.is_dir()
         assert len(list(config_dir.iterdir())) == 0
+
         config_dir.rmdir()
 
     def test_creation_of_infered_config_directory(self, monkeypatch):
+        """When no directory is specified, it is inferred instead."""
         config_path = Path('/tmp/astrality_config')
         monkeypatch.setattr(
             os,
